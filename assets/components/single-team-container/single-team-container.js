eb3f22d5971c22c64a4b2363055f3b3a79b02622
KostasRafael <korafamanou@gmail.com>
2025-11-10 13:31:02 +0200

    updated renderTeams() function, and displayTeamFixtures() function

--- assets/components/single-team-container/single-team-container.js
+++ assets/components/single-team-container/single-team-container.js
@@ -4,15 +4,14 @@ class singleTeamContainer extends HTMLElement {
   }
 
   connectedCallback() {
+    const teamId = this.getAttribute("team-id");
     const teamName = this.getAttribute("team-name");
     const teamLogo = this.getAttribute("team-logo");
 
     console.log("lo", teamLogo);
 
     this.innerHTML = `
-          <button class="team-logo-name-div" onclick="displayTeamFixtures(${teamName
-            .replace(/\s+/g, "")
-            .toLowerCase()}Fixtures)">
+          <button class="team-logo-name-div" onclick="displayTeamFixtures(${teamId})">
             <div class="logo-name">  
                 <h4 class="team-name"> <img src="${teamLogo}" alt="" class="team-logo">  ${
       teamName.split(" ")[0]
--- functions.js
+++ functions.js
@@ -1,71 +1,75 @@
-async function fetchAndSaveOneTeamsTenFixtures(team) {
-  const nameOfCurrentTeam = team.name;
-  const idOfCurrentTeam = team.id;
+async function fetchTeams(leagueId) {
+  const url = `https://v3.football.api-sports.io/teams?league=${leagueId}&season=2025`;
   const headers = {
     "x-rapidapi-host": "v3.football.api-sports.io",
     "x-rapidapi-key": "94d01bff67718f85de3f0532069fbbf1",
   };
-  const url = `https://v3.football.api-sports.io/fixtures?team=${idOfCurrentTeam}&league=197&season=2025&last=10`;
+
   const response = await fetch(url, { headers });
-  if (!response.ok) throw new Error(`${team.name} failed: ${response.status}`);
-  const data = await response.json();
-  if (team.name === "olympiakos") {
-    olympiakosFixtures = data.response;
-  } else if (team.name === "aek") {
-    aekFixtures = data.response;
-  } else if (team.name === "panathinaikos") {
-    panathinaikosFixtures = data.response;
-  } else if (team.name === "larissa") {
-    larissaFixtures = data.response;
-  } else if (team.name === "paok") {
-    paokFixtures = data.response;
-  } else if (team.name === "asteras") {
-    asterasFixtures = data.response;
-  } else if (team.name === "aris") {
-    arisFixtures = data.response;
-  } else if (team.name === "ofi") {
-    ofiFixtures = data.response;
-  } else if (team.name === "levadiakos") {
-    levadiakosFixtures = data.response;
-  } else if (team.name === "panseraikos") {
-    panseraikosFixtures = data.response;
-  } else if (team.name === "kifisia") {
-    kifisiaFixtures = data.response;
-  } else if (team.name === "atromitos") {
-    atromitosFixtures = data.response;
-  } else if (team.name === "panetolikos") {
-    panetolikosFixtures = data.response;
-  } else if (team.name === "volos") {
-    volosFixtures = data.response;
-  } else {
-    console.log("No corresponding variable found");
-  }
+  console.log("response", response);
+  const responseJson = await response.json();
+  console.log("responseJson", responseJson);
+  const arrayOfTeams = responseJson.response;
+  console.log("arrayOfTeams", arrayOfTeams);
+  return arrayOfTeams;
 }
 
-async function fetchAllTeamsSequentially() {
-  for (const team of allTeams) {
+async function useAllTeams(leagueId) {
+  const allTeamsArray = await fetchTeams(leagueId);
+  console.log("allTeamsArray", allTeamsArray);
+  const allTeams = allTeamsArray.reduce((accumulator, teamItem) => {
+    accumulator[teamItem.team.id] = {
+      name: teamItem.team.name,
+      id: teamItem.team.id,
+      logo: teamItem.team.logo,
+      fixturesArray: [],
+    };
+    return accumulator;
+  }, {});
+
+  console.log("allTeams", allTeams);
+  return allTeams;
+}
+
+async function readyAllTeams(leagueId) {
+  const result = await useAllTeams(leagueId);
+  const allLeagueTeams = result;
+  return allLeagueTeams;
+}
+
+//------------------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------------------//
+
+// teamId: extracted from the team argument available when allTeams.forEach((team) => {  })
+// LeagueId: passed on function call within the html page
+const urlFixtures = `https://v3.football.api-sports.io/fixtures?team=${idOfCurrentTeam}&league=${someLeagueId}&season=2025&last=10`;
+
+/*
+async function fetchAndSaveTeamsFixtures(someTeamKey, someLeagueId) {
+  const team = allTeams[someTeamKey];
+  const idOfCurrentTeam = team.id;
+  const urlFixtures = `https://v3.football.api-sports.io/fixtures?team=${idOfCurrentTeam}&league=${someLeagueId}&season=2025&last=10`;
+  const responseFixtures = await fetch(urlFixtures, { headers });
+  if (!responseFixtures.ok)
+    throw new Error(`${team.name} failed: ${responseFixtures.status}`);
+  const fixtures = await responseFixtures.json();
+  team.fixturesArray = fixtures;
+  console.log("TeamFixturesUpdated", team);
+}
+
+async function fetchAllTeamsFixturesSequentially(someLeagueId) {
+  for (const someTeamKey in allTeams) {
     try {
-      await fetchAndSaveOneTeamsTenFixtures(team);
+      await fetchAndSaveTeamsFixtures(someTeamKey, someLeagueId);
       console.log("fetched team!");
     } catch (err) {
-      console.error(`Error fetching ${team.name}:`, err);
+      console.error(`Error fetching ${someTeamKey}:`, err);
     }
   }
 }
 
-async function fetchAndSaveAllTeamsFixtures() {
-  await fetchAllTeamsSequentially();
-}
-
-function fetchAllTeamsOfLeague() {
-  fetch("https://v3.football.api-sports.io/teams?league=78&season=2025", {
-    headers,
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const allteams = data.response;
-      console.log("allTeams", allteams);
-    });
+async function fetchAndSaveAllTeamsFixtures(someLeagueId) {
+  await fetchAllTeamsFixturesSequentially(someLeagueId);
 }
 
 // Next Fixtures
@@ -124,3 +128,17 @@ function fetchNextFixtures(leagueId) {
       });
     });
 }
+
+function fetchAllTeamsOfLeague(leagueId) {
+  fetch(
+    `https://v3.football.api-sports.io/teams?league=${leagueId}&season=2025`,
+    {
+      headers,
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const allteams = data.response;
+      console.log("allTeams", allteams);
+    });
+} */
